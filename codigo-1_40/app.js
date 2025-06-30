cb432e3628448833a9f40fb055097ae82caad3ee
Cristopher Ordoñez <cordonezc4@est.ups.edu.ec>
2025-06-30 16:19:15 -0500

    Commit inicial

--- codigo-1_40/app.js
+++
@@ -1,8 +0,0 @@
-// Mostrar una alerta automática por unos segundos
-$(document).ready(function() {
-    const alerta = document.getElementById("alertaImportante");
-    setTimeout(() => {
-      alerta.classList.add("d-none");
-    }, 6000); // Ocultar después de 6 segundos
-  });
-  
